20ee854ed3ff3c85a4b3af3930455d8994186f9a
driessenslucas <lucas.driessens@student.howest.be>
2024-01-28 18:25:52 +0100

    demo

---
+++ README.md
@@ -0,0 +1,11 @@
+# RPI GPIO over HTTP with Docker
+
+This is a simple HTTP server that allows you to control the GPIO pins of a Raspberry Pi.
+
+## Usage
+
+- clone this repo
+
+```bash
+
+```
---
+++ rpi5_gpio_over_docker_demo/sensors/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, Response, render_template
+from gpiozero import DistanceSensor
+from time import sleep
+from signal import pause
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+   return "hello_world 2"
+
+@app.route('/sensor/')
+def get_sensor_value():
+      try:
+        hc_sr04 = DistanceSensor(echo=27, trigger=17)
+        distance = hc_sr04.distance * 100
+        sleep(0.1)
+        return f"distance: {distance:.2f} cm"
+
+      except Exception as e:
+         print(f"Error: {e}")
+         return "Error reading sensor"
+
+if __name__ == '__main__':
+  app.run(debug=True, host="0.0.0.0", port='5500')
